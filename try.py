8b64bce0ff80aab9a2c36be48231ad3c0968443c
Rohith Vijayan <106431458+rohithvijayan@users.noreply.github.com>
2024-07-13 23:34:42 +0530

    Delete try.py

--- try.py
+++
@@ -1,4 +0,0 @@
-from gtts import gTTS
-
-tts = gTTS(text=" I'm sorry to hear that you're going through this difficult time. It's completely normal to feel homesick, especially when you're away from your loved ones for an extended period. However, if you're trying to convince your girlfriend not to break up with you, it might be helpful to focus on the positive aspects of your relationship and how much you value her in your life. Communication is key, so try to have an open and honest conversation with her",tld='co.in')
-tts.save("hello.mp3")
